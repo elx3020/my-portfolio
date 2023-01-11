4da356e2a12513cd41dd77ca2a5866b755dce0bc
Esteban Lasso <estebannl28@gmail.com>
2023-01-11 23:28:55 +0100

    global types

---
+++ src/global.d.ts
@@ -0,0 +1,42 @@
+
+// to have global types do not use any import or export statement in the file
+type DataIndentity = {
+    id: string,
+    arr_Id: string,
+    project_Name: string,
+}
+
+type ProjectDataT = DataIndentity & {
+    description: string,
+    project_url: string,
+    image_Url?: string,
+    imageAlt?: string
+}
+
+type UIProjectDataT = ProjectDataT & {
+    imageCollection: string[],
+
+}
+
+type GlobalDataT = {
+    showcase_Data: ProjectDataT[],
+    mini_projects: ProjectDataT[],
+    web_Proj_Large: ProjectDataT[],
+    web_Proj_Mini: ProjectDataT[],
+    ui_projects: UIProjectDataT[],
+
+}
+
+type ClassMode = 'single' | 'multiple'
+
+type ComponentBT<T extends ClassMode> = {
+    className: T extends 'single' ? string : string[],
+    image_Url?: string,
+    title?: string,
+    textContent?: string,
+    children?: any
+    styleOptions?: React.CSSProperties
+}
+
+
+
