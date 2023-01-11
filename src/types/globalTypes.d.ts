9802be8b844fdd42052e73c950d98887a4576926
Esteban Lasso <estebannl28@gmail.com>
2023-01-11 23:28:01 +0100

    remove old types

--- src/types/globalTypes.d.ts
+++ src/types/globalTypes.d.ts
@@ -1,41 +1,41 @@
-export type DataIndentity = {
-    id: string, 
-    arr_Id: string, 
-    project_Name: string, 
-}
+// export type DataIndentity = {
+//     id: string, 
+//     arr_Id: string, 
+//     project_Name: string, 
+// }
 
-export type ProjectDataT = DataIndentity & {
-    description: string, 
-    project_url: string, 
-    image_Url?: string,
-    imageAlt?: string
-}
+// export type ProjectDataT = DataIndentity & {
+//     description: string, 
+//     project_url: string, 
+//     image_Url?: string,
+//     imageAlt?: string
+// }
 
-export type UIProjectDataT = ProjectDataT & {
-    imageCollection: string[],
+// export type UIProjectDataT = ProjectDataT & {
+//     imageCollection: string[],
 
-}
+// }
 
 
 
-export type GlobalDataT = {
-    showcase_Data: ProjectDataT[], 
-    mini_projects: ProjectDataT[],  
-    web_Proj_Large: ProjectDataT[], 
-    web_Proj_Mini: ProjectDataT[],
-    ui_projects: UIProjectDataT[],
+// export type GlobalDataT = {
+//     showcase_Data: ProjectDataT[], 
+//     mini_projects: ProjectDataT[],  
+//     web_Proj_Large: ProjectDataT[], 
+//     web_Proj_Mini: ProjectDataT[],
+//     ui_projects: UIProjectDataT[],
     
-}
+// }
 
-type ClassMode = 'single' | 'multiple'
+// type ClassMode = 'single' | 'multiple'
 
-export type ComponentBT<T extends ClassMode> = {
-    className: T extends 'single' ? string : string[] ,
-    image_Url?: string,
-    title?: string,
-    textContent?: string,
-    children?: any 
-    styleOptions?: React.CSSProperties 
-}
+// export type ComponentBT<T extends ClassMode> = {
+//     className: T extends 'single' ? string : string[] ,
+//     image_Url?: string,
+//     title?: string,
+//     textContent?: string,
+//     children?: any 
+//     styleOptions?: React.CSSProperties 
+// }
 
 
