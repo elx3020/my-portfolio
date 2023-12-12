6b9ca2989df5323d63016e0998d6e864754989f0
Esteban Lasso <36430021+elx3020@users.noreply.github.com>
2023-12-12 23:33:10 +0100

    style updates

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../hooks/useGlobalContext";
 
-export default function Card(props: {projectData: ProjectDataT}) {
-  const {
-    project_Name,
-    image_Url,
-    description,
-    project_url,
-    id,
-    arr_Id,
-  } = props.projectData;
+export default function Card(props: { projectData: ProjectDataT }) {
+  const { project_Name, image_Url, description, project_url, id, arr_Id } =
+    props.projectData;
 
   let navigate = useNavigate();
 
   const globalContext = useGlobalContext();
-  const buttonText = globalContext.content.workPage.buttons.button_card
+  const buttonText = globalContext.content.workPage.buttons.button_card;
 
   const handleClick = () => {
     navigate(`projects/${arr_Id}/${id}`);
@@ -23,15 +17,17 @@ export default function Card(props: {projectData: ProjectDataT}) {
   const projectLink = `/projects/${arr_Id}/${id}`;
 
   return (
-    <div data-scroll data-scroll-offset="50%" className="card-showcase">
-      <div className="image-container ">
-        <img src={image_Url} alt={image_Url}></img>
-        <div className="overlay"></div>
-      </div>
-      <div className="card-content">
-        <h1>{project_Name}</h1>
-        <p>{description}</p>
-        <a href={project_url}>{buttonText}</a>
+    <div data-scroll data-scroll-offset="50%" className="card-showcase" >
+      <div className="c_h">
+        <div className="image-container ">
+          <img src={image_Url} alt={image_Url}></img>
+          <div className="overlay"></div>
+        </div>
+        <div className="card-content">
+          <h1>{project_Name}</h1>
+          <p>{description}</p>
+          <a href={project_url}>{buttonText}</a>
+        </div>
       </div>
     </div>
   );
