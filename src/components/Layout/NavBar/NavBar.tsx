5ce6e30d3a7d302228ad850f08d4c4b61d5ef95c
Esteban Lasso <36430021+elx3020@users.noreply.github.com>
2023-12-12 22:23:07 +0100

    nav panel language

--- src/components/Layout/NavBar/NavBar.tsx
+++ src/components/Layout/NavBar/NavBar.tsx
@@ -2,24 +2,42 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Navbar.sass";
 import React, { useState } from "react";
 import { useGlobalContext } from "../../../hooks/useGlobalContext";
-const Navbar = (props: { scrollDirection: any}) => {
+const Navbar = (props: { scrollDirection: any }) => {
   const { scrollDirection } = props;
 
   const location = useLocation();
   const navigate = useNavigate();
   const globalContext = useGlobalContext();
 
-  let navTextContent = { home: '', work: '', studies: '', about: '', contact: '' }
-  if (globalContext.language === 'en') { 
-    navTextContent = { home: 'Home', work: 'Work', studies: 'Studies', about: 'About', contact: 'Contact' };
-  } else if (globalContext.language === 'es') { 
-    navTextContent = { home: 'Inicio', work: 'Trabajo', studies: 'Estudios', about: 'Acerca de', contact: 'Contacto' };
+  let navTextContent = {
+    home: "",
+    work: "",
+    studies: "",
+    about: "",
+    contact: "",
+  };
+  if (globalContext.language === "en") {
+    navTextContent = {
+      home: "Home",
+      work: "Work",
+      studies: "Studies",
+      about: "About",
+      contact: "Contact",
+    };
+  } else if (globalContext.language === "es") {
+    navTextContent = {
+      home: "Inicio",
+      work: "Trabajo",
+      studies: "Estudios",
+      about: "Acerca de",
+      contact: "Contacto",
+    };
   }
 
   const changeLanguage = (value) => {
     globalContext.setLenguage(value);
     closeDropdown();
-  }
+  };
 
   const [isOpen, setIsOpen] = useState(false);
   const toggleDropdown = () => {
@@ -32,30 +50,28 @@ const Navbar = (props: { scrollDirection: any}) => {
 
   const links = (
     <div className="nav_links">
-      <Link to="work">{navTextContent.work}</Link> 
+      <Link to="work">{navTextContent.work}</Link>
       {/* <Link to="studies">Studies</Link>  */}
       <Link to="about">{navTextContent.about}</Link>
       <Link to="contact">{navTextContent.contact}</Link>
-      
-     
     </div>
   );
 
   return (
-    <nav id="navbar" className={`Navbar ${scrollDirection}`} >
+    <nav id="navbar" className={`Navbar ${scrollDirection}`}>
       <div className="nav_links">
-        <Link to="/">{ navTextContent.home}</Link>
+        <Link to="/">{navTextContent.home}</Link>
       </div>
       {links}
-      <div className={`custom-dropdown `}>
-      <div className="selected-option" onClick={toggleDropdown}>
-        {globalContext.language === 'en' ? 'English' : 'Español'}
+      <div className={`custom-dropdown `}  onMouseLeave={toggleDropdown}>
+        <div className="selected-option" onMouseEnter={toggleDropdown} >
+          {globalContext.language === "en" ? "Language" : "Lenguaje"}
+        </div>
+        <div className={`options ${isOpen ? "open" : ""}`}>
+          <div onClick={() => changeLanguage("en")}>English</div>
+          <div onClick={() => changeLanguage("es")}>Español</div>
+        </div>
       </div>
-      <div className={`options ${isOpen ? 'open' : ''}`}>
-        <div onClick={() => changeLanguage('en')}>English</div>
-        <div onClick={() => changeLanguage('es')}>Español</div>
-      </div>
-    </div>
     </nav>
   );
 };
--- src/components/Layout/NavBarPanel/NavBarPanel.tsx
+++ src/components/Layout/NavBarPanel/NavBarPanel.tsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import "./NavPanel.sass";
+import { useGlobalContext } from "../../../hooks/useGlobalContext";
 
 export default function NavBarPanel(props: {currentPage: string, scrollDirection: string}) {
   const [navPanelOpen, setNavPanel] = useState("close");
 
   const { currentPage, scrollDirection } = props;
+
+  
 
   function togglePanel(e: React.MouseEvent ) {
     if (navPanelOpen === "close") {
@@ -34,13 +37,26 @@ export default function NavBarPanel(props: {currentPage: string, scrollDirection
   );
 }
 
-function NavPanel(props: {toggleClass: string}) {
+function NavPanel(props: { toggleClass: string }) {
+  
+
+  const globalContext = useGlobalContext();
+  let navBarContent = { home: '', work: '', about: '', contact: '', language: ''}	
+
+  if (globalContext.language === "en") {
+    navBarContent = { home: 'Home', work: 'My Work', about: 'About me', contact: 'Contact', language: 'Language' }
+  } else if (globalContext.language === "es") { 
+    navBarContent = { home: 'Inicio', work: 'Mi Trabajo', about: 'Sobre mi', contact: 'Contacto', language: 'Idioma' }
+  }
+
   return (
     <div id="navpanel" className={`nav-panel ${props.toggleClass}`}>
-      <Link to="/">Home</Link>
-      <Link to="work">My Work</Link>
-      <Link to="about">About me</Link>
-      <Link to="contact">Contact</Link>
+      <Link to="/">{navBarContent.home}</Link>
+      <Link to="work">{navBarContent.work}</Link>
+      <Link to="about">{navBarContent.about}</Link>
+      <Link to="contact">{navBarContent.contact}</Link>
+      <div>{navBarContent.language} <span onClick={(e) => {globalContext.setLenguage('en')}}>EN</span> <span>/</span> <span onClick={(e) => {globalContext.setLenguage('es')}}>ES</span></div>
+
     </div>
   );
 }
